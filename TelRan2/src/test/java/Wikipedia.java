b310a4cbb1cefcfd121f5e2f1d6df258bc5ae1b8
Asniga <asniga@gmail.com>
2019-01-22 19:53:04 +0200

    change

--- TelRan2/src/test/java/Wikipedia.java
+++ TelRan2/src/test/java/Wikipedia.java
@@ -1,26 +1,36 @@
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class Wikipedia {
-    Webdriver = wd;
+    WebDriver wd;
 
     @BeforeClass
     public void setUp (){
         wd=new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS); // wait max 3 sec dlya poiska elementov, idet dal'she
+        //wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS); // wait max 3 sec dlya poiska elementov, idet dal'she
     }
 
     @Test
-    public void chromeBrowserTest(){
-        wd.get("https://www.ebay.com/");
+    public void chromeBrowserTest() throws InterruptedException {
+        wd.get("https://www.wikipedia.org/");
         // type in SearchBar
-        wd.findElement(By.id("gh-ac")).click();
-        wd.findElement(By.id("gh-ac")).clear();
-        wd.findElement(By.id("gh-ac")).sendKeys("Shoes");
-        wd.findElement(By.id("gh-btn")).click();
+        wd.findElement(By.id("js-link-box-en")).click();
+        Thread.sleep(1000);
+        wd.findElement(By.id("pt-login")).click();
+        Thread.sleep(1000);
+        wd.findElement(By.id("wpName1")).clear();
+        wd.findElement(By.id("wpName1")).sendKeys("Anna");
+        wd.findElement(By.id("wpPassword1")).clear();
+        wd.findElement(By.id("wpPassword1")).click();
+        wd.findElement(By.id("wpPassword1")).sendKeys("123456");
+
+        wd.findElement(By.id("wpLoginAttempt")).click();
     }
 
     @AfterClass
