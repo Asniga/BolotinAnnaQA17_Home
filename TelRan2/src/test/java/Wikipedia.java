de3824271cadf1947727756f08dd0315e2c76833
Asniga <asniga@gmail.com>
2019-01-25 16:24:32 +0200

    Refactor

--- TelRan2/src/test/java/Wikipedia.java
+++ TelRan2/src/test/java/Wikipedia.java
@@ -13,24 +13,60 @@ public class Wikipedia {
     @BeforeClass
     public void setUp (){
         wd=new ChromeDriver();
-        //wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS); // wait max 3 sec dlya poiska elementov, idet dal'she
+        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS); // wait max 3 sec dlya poiska elementov, idet dal'she
+        wd.manage().window().maximize();
     }
 
     @Test
     public void chromeBrowserTest() throws InterruptedException {
-        wd.get("https://www.wikipedia.org/");
-        // type in SearchBar
-        wd.findElement(By.id("js-link-box-en")).click();
-        Thread.sleep(1000);
-        wd.findElement(By.id("pt-login")).click();
-        Thread.sleep(1000);
-        wd.findElement(By.id("wpName1")).clear();
-        wd.findElement(By.id("wpName1")).sendKeys("Anna");
+        openSite("https://www.wikipedia.org/");
+        clickEnglish("js-link-box-en");
+        clickLogin("pt-login");
+        insertName("Asniga");
+        insertPass("dtcyf1235");
+        clickOk();
+        serch("Game of Thrones");
+        addToWatchList();
+    }
+
+    public void addToWatchList() throws InterruptedException {
+        wd.findElement(By.cssSelector("[accesskey=w]")).click();
+        Thread.sleep(5000);
+    }
+
+    public void serch(String s) {
+        wd.findElement(By.id("searchInput")).click();
+        wd.findElement(By.id("searchInput")).clear();
+        wd.findElement(By.id("searchInput")).sendKeys(s);
+
+    }
+
+    public void clickOk() {
+        wd.findElement(By.id("wpLoginAttempt")).click();
+    }
+
+    public void insertPass(String s) {
         wd.findElement(By.id("wpPassword1")).clear();
         wd.findElement(By.id("wpPassword1")).click();
-        wd.findElement(By.id("wpPassword1")).sendKeys("123456");
+        wd.findElement(By.id("wpPassword1")).sendKeys(s);
+    }
 
-        wd.findElement(By.id("wpLoginAttempt")).click();
+    public void insertName(String anna) {
+        wd.findElement(By.id("wpName1")).clear();
+        wd.findElement(By.id("wpName1")).sendKeys(anna);
+    }
+
+    public void clickLogin(String s) throws InterruptedException {
+        wd.findElement(By.id(s)).click();
+        Thread.sleep(1000);
+    }
+
+    public void clickEnglish(String s) throws InterruptedException {
+        clickLogin(s);
+    }
+
+    public void openSite(String url) {
+        wd.get(url);
     }
 
     @AfterClass
